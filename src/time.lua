2614cda69534d2596c04f693f5c7ac67c9d4fe54
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-25 16:19:03 +0300

    Plugin styled fix. Internet available sync added.

--- src/time.lua
+++ src/time.lua
@@ -1,21 +1,22 @@
-local time = {
-    sync = function(callback)
-        sntp.sync("0.ua.pool.ntp.org", function(sec, msec, server)
-            rtctime.set(sec, msec)
+local time = {}
 
-            if callback ~= nil then
-                callback(time.getTime())
-            end
-        end)
-    end,
-    getTime = function()
-        local sec = rtctime.get()
-        return {
-            hour = math.floor(sec % 86400 / 3600 + 3),
-            minute = math.floor(sec % 3600 / 60),
-            second = math.floor(sec % 60)
-        }
-    end
-}
+function time.sync()
+    sntp.sync("0.ua.pool.ntp.org", function(sec, msec, server)
+        rtctime.set(sec, msec)
+        print("TIME SYNC")
+    end)
+end
 
-return time
+function time.getTime()
+    local sec = rtctime.get()
+    return {
+        hour = math.floor(sec % 86400 / 3600 + 3),
+        minute = math.floor(sec % 3600 / 60),
+        second = math.floor(sec % 60)
+    }
+end
+
+return function (app)
+    app.modules.internet.on('appeared', time.sync)
+    return time
+end
