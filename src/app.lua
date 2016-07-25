479da9181dce7ffbbf8798dc4ad8e10b5151cb0f
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-25 15:38:56 +0300

    Loading modules system added.

--- src/app.lua
+++ src/app.lua
@@ -1,85 +1,114 @@
-local config = {}
+local app = {}
+app.config = {}
+app.listeners = {}
+app.listeners.modulesloaded = {}
+app.modules = {}
 
-function init(conf)
-    config = conf
+function app.init(conf)
+    app.config = conf and conf or {}
+    local status, error = pcall(app.loadModules)
 
-    if pcall(loadModules) then
+    if status then
         print("MODULES LOADED")
+
+        app.modules.internet.on('appeared', function (i)
+            print("IP:" .. i.IP)
+        end)
+
+        for i,callback in pairs(app.listeners.modulesloaded) do
+            status, error = pcall(callback, app)
+
+            if not status then
+                print(error)
+            end
+        end
+    else
+        print("app", "ERROR MODULES LOAD", error)
     end
 
-    print("WAKEUP WIFI")
-    wifi.setmode(wifi.STATION)
-    wifi.sta.config(config.data.wifiw.ssid, config.data.wifiw.pass, 1)
+    --    print("WAKEUP WIFI")
+    --    wifi.setmode(wifi.STATION)
+    --    wifi.sta.config(config.data.wifiw.ssid, config.data.wifiw.pass, 1)
 
---    local function wifi_connect()
---        if wifi.sta.getip() ~= nil then
---            tmr.stop(0)
---
---            time.sync(function(now)
---                print(string.format("NOW: %02d:%02d:%02d", now.hour, now.minute, now.second))
---            end)
---
---            print("IP: " .. wifi.sta.getip())
---        end
---    end
---
---    local function ledssync()
---        now = time.getTime()
---
---        if (now.hour == astro.data.sunrise.hour and now.minute >= astro.data.sunrise.minute) or
---                (now.hour > astro.data.sunrise.hour and now.hour < astro.data.sunset.hour) or
---                (now.hour == astro.data.sunset.hour and now.minute <= astro.data.sunset.minute) then
---            if light.phase.curr ~= "sun" then
---                light.phase = { last = light.phase.curr, curr = "sun" }
---            end
---        elseif (now.hour == astro.data.moonrise.hour and now.minute >= astro.data.moonrise.minute) or
---                (now.hour > astro.data.moonrise.hour or now.hour < astro.data.moonset.hour) or
---                (now.hour == astro.data.moonset.hour and now.minute <= astro.data.moonset.minute) then
---            if light.phase.curr ~= "moon" then
---                light.phase = { last = light.phase.curr, curr = "moon" }
---            end
---        elseif light.phase.curr ~= "night" then
---            light.phase = { last = light.phase.curr, curr = "night" }
---        end
---
---        tmr.unregister(props.timers.trans.id)
---        tmr.alarm(props.timers.trans.id, props.timers.trans.interval, tmr.ALARM_SINGLE, check)
---    end
---
---    local function check()
---        if light.phase.last ~= light.phase.curr then
---            print(light.phase.last, "->", light.phase.curr)
---
---            light.phase.last = light.phase.curr
---
---            if light.phase.curr == "sun" then
---                light.transition.day()
---            elseif light.phase.curr == "moon" then
---                light.transition.moon()
---            else
---                light.transition.night()
---            end
---        end
---    end
+    --    local function wifi_connect()
+    --        if wifi.sta.getip() ~= nil then
+    --            tmr.stop(0)
+    --
+    --            time.sync(function(now)
+    --                print(string.format("NOW: %02d:%02d:%02d", now.hour, now.minute, now.second))
+    --            end)
+    --
+    --            print("IP: " .. wifi.sta.getip())
+    --        end
+    --    end
+    --
+    --    local function ledssync()
+    --        now = time.getTime()
+    --
+    --        if (now.hour == astro.data.sunrise.hour and now.minute >= astro.data.sunrise.minute) or
+    --                (now.hour > astro.data.sunrise.hour and now.hour < astro.data.sunset.hour) or
+    --                (now.hour == astro.data.sunset.hour and now.minute <= astro.data.sunset.minute) then
+    --            if light.phase.curr ~= "sun" then
+    --                light.phase = { last = light.phase.curr, curr = "sun" }
+    --            end
+    --        elseif (now.hour == astro.data.moonrise.hour and now.minute >= astro.data.moonrise.minute) or
+    --                (now.hour > astro.data.moonrise.hour or now.hour < astro.data.moonset.hour) or
+    --                (now.hour == astro.data.moonset.hour and now.minute <= astro.data.moonset.minute) then
+    --            if light.phase.curr ~= "moon" then
+    --                light.phase = { last = light.phase.curr, curr = "moon" }
+    --            end
+    --        elseif light.phase.curr ~= "night" then
+    --            light.phase = { last = light.phase.curr, curr = "night" }
+    --        end
+    --
+    --        tmr.unregister(props.timers.trans.id)
+    --        tmr.alarm(props.timers.trans.id, props.timers.trans.interval, tmr.ALARM_SINGLE, check)
+    --    end
+    --
+    --    local function check()
+    --        if light.phase.last ~= light.phase.curr then
+    --            print(light.phase.last, "->", light.phase.curr)
+    --
+    --            light.phase.last = light.phase.curr
+    --
+    --            if light.phase.curr == "sun" then
+    --                light.transition.day()
+    --            elseif light.phase.curr == "moon" then
+    --                light.transition.moon()
+    --            else
+    --                light.transition.night()
+    --            end
+    --        end
+    --    end
 end
 
-function loadModules()
-    for i,v in ipairs(config.data.modules) do
-        print("load" .. i .. ": " .. v)
-    end
+function app.loadModules()
+    print("LOAD MODULES:", cjson.encode(app.config.props.modules))
+    for i, name in pairs(app.config.props.modules) do
+        print("LOAD MODULE:", name)
+        local status, module = pcall(dofile, name .. ".lua")
 
---    print("LOAD PROPS")
---    props = dofile("props.lua")
---    print("LOAD LIGHT")
---    light = dofile("light.lua")
---    light.init()
---    print("LOAD TIME")
---    time = dofile("time.lua")
---    print("LOAD ASTRO")
---    astro = dofile("astro.lua")
---    print("LOAD WEB")
---    web = dofile("web.lua")
---    web.init()
+        if status then
+            -- CALL MODULE INIT FUNCTION
+            status, module = pcall(module, app)
+
+            if status then
+                app.modules[name] = module
+            else
+                print("app", "ERROR INIT MODULE", name, module)
+            end
+        else
+            print("app", "ERROR LOAD MODULE", name, module)
+        end
+    end
+end
+
+function app.on(event, callback)
+    if event == "modulesloaded" then
+        table.insert(app.listeners.modulesloaded, callback)
+    else
+        print("EVENT DOESN'T EXISTS", event)
+    end
 end
 
 --tmr.alarm(0, 500, tmr.ALARM_AUTO, wifi_connect)
@@ -87,4 +116,4 @@ end
 --tmr.alarm(props.timers.astro.id, props.timers.astro.interval, tmr.ALARM_AUTO, astro.sync)
 --tmr.alarm(props.timers.light.id, props.timers.light.interval, tmr.ALARM_AUTO, ledssync)
 
-return init
+return app.init
