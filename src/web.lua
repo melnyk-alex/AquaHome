69338cb89c56230da6db5bd1bc477eb071598975
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-25 16:03:20 +0300

    Push app config to html added.

--- src/web.lua
+++ src/web.lua
@@ -1,48 +1,57 @@
+local application
 local web = {
     server = nil,
     opened = false,
-    init = function()
-        web.server = net.createServer(net.TCP, 5)
-        web.server:listen(80, web.handler)
-    end,
-    handler = function(conn)
-        conn:on("receive", function(sock, data)
-            filename = string.match(data, "GET /(.*) HTTP")
-            filename = filename == "" and "index.html" or filename
+    jsonprops = ""
+}
 
-            if file.exists(filename) then
-                file.open(filename, "r")
-                opened = true
-                sock:send("HTTP/1.1 200 OK\r\nServer: AquaHome on ESP8266\r\nContent-Type: text/html\r\n\r\n")
+function web.handler(conn)
+    conn:on("receive", function(sock, data)
+        ok, json = pcall(cjson.encode, application.config.props)
+        if ok then
+            web.jsonprops = json
+        end
+
+        filename = string.match(data, "GET /(.*) HTTP")
+        filename = filename == "" and "index.html" or filename
+
+        if file.exists(filename) then
+            file.open(filename, "r")
+            opened = true
+            sock:send("HTTP/1.1 200 OK\r\nServer: AquaHome on ESP8266\r\nContent-Type: text/html\r\n\r\n")
+        else
+            sock:send("HTTP/1.1 404 OK\r\nServer: AquaHome on ESP8266\r\nContent-Type: text/html\r\n\r\n")
+        end
+    end)
+
+    local send = function(sock)
+        if opened then
+            local line = file.readline()
+
+            if line ~= nil then
+                local wrapped = string.gsub(line, "//APP_CONFIG", "var config = " .. web.jsonprops .. ";")
+
+                print(wrapped)
+
+                sock:send(wrapped)
             else
-                sock:send("HTTP/1.1 404 OK\r\nServer: AquaHome on ESP8266\r\nContent-Type: text/html\r\n\r\n")
-            end
-        end)
-
-        local send = function(sock)
-            if opened then
-                local line = file.readline()
-
-                if line ~= nil then
-                    ok, json = pcall(cjson.encode, {})
-                    if ok then
-                        line = string.gsub(line, "JS", "var data = " .. json .. ";")
-                    end
-
-                    sock:send(line)
-                else
-                    file.close()
-                    opened = false
-                end
-            end
-
-            if not opened then
-                sock:close()
+                file.close()
+                opened = false
             end
         end
 
-        conn:on("sent", send)
+        if not opened then
+            sock:close()
+        end
     end
-}
 
-return web
+    conn:on("sent", send)
+end
+
+return function(app)
+    application = app
+    web.server = net.createServer(net.TCP, 5)
+    web.server:listen(80, web.handler)
+
+    return web
+end
