40721f3500e75656018c0de6a9ceef219c94eb11
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-25 16:24:11 +0300

    Refactoring.

--- src/light.lua
+++ src/light.lua
@@ -12,55 +12,57 @@ local light = {
         moon = { b = 60, c = 15, h = 25 },
         night = 0
     },
-    phase = { last = "", curr = "" },
-    transition = {
-        day = function()
-            print("DAY MODE")
-            for i = 0, light.const.max do
-                light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.const.min
-                light.leds.c.bright = light.leds.c.bright < light.bright.day and light.leds.c.bright + 1 or light.bright.day
-                light.leds.h.bright = light.leds.h.bright < light.bright.day and light.leds.h.bright + 1 or light.bright.day
-
-                pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-                pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-                pwm.setduty(light.leds.h.pin, light.leds.h.bright)
-
-                tmr.delay(50)
-            end
-            print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
-        end,
-        moon = function()
-            print("MOON MODE")
-            for i = 0, light.const.max do
-                light.leds.b.bright = light.leds.b.bright < light.bright.moon.b and light.leds.b.bright + 1 or light.bright.moon.b
-                light.leds.c.bright = light.leds.c.bright > light.bright.moon.c and light.leds.c.bright - 1 or light.bright.moon.c
-                light.leds.h.bright = light.leds.h.bright > light.bright.moon.h and light.leds.h.bright - 1 or light.bright.moon.h
-
-                pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-                pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-                pwm.setduty(light.leds.h.pin, light.leds.h.bright)
-
-                tmr.delay(50)
-            end
-            print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
-        end,
-        night = function()
-            print("NIGHT MODE")
-            for i = 0, light.const.max do
-                light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.bright.night
-                light.leds.c.bright = light.leds.c.bright > light.const.min and light.leds.c.bright - 1 or light.bright.night
-                light.leds.h.bright = light.leds.h.bright > light.const.min and light.leds.h.bright - 1 or light.bright.night
-
-                pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-                pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-                pwm.setduty(light.leds.h.pin, light.leds.h.bright)
-
-                tmr.delay(50)
-            end
-            print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
-        end
-    }
+    phase = { last = "", curr = "" }
 }
+light.transition = {}
+
+function light.transition.day()
+    print("DAY MODE")
+    for i = 0, light.const.max do
+        light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.const.min
+        light.leds.c.bright = light.leds.c.bright < light.bright.day and light.leds.c.bright + 1 or light.bright.day
+        light.leds.h.bright = light.leds.h.bright < light.bright.day and light.leds.h.bright + 1 or light.bright.day
+
+        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
+        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
+        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
+
+        tmr.delay(50)
+    end
+    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
+end
+
+function light.transition.moon()
+    print("MOON MODE")
+    for i = 0, light.const.max do
+        light.leds.b.bright = light.leds.b.bright < light.bright.moon.b and light.leds.b.bright + 1 or light.bright.moon.b
+        light.leds.c.bright = light.leds.c.bright > light.bright.moon.c and light.leds.c.bright - 1 or light.bright.moon.c
+        light.leds.h.bright = light.leds.h.bright > light.bright.moon.h and light.leds.h.bright - 1 or light.bright.moon.h
+
+        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
+        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
+        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
+
+        tmr.delay(50)
+    end
+    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
+end
+
+function light.transition.night()
+    print("NIGHT MODE")
+    for i = 0, light.const.max do
+        light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.bright.night
+        light.leds.c.bright = light.leds.c.bright > light.const.min and light.leds.c.bright - 1 or light.bright.night
+        light.leds.h.bright = light.leds.h.bright > light.const.min and light.leds.h.bright - 1 or light.bright.night
+
+        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
+        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
+        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
+
+        tmr.delay(50)
+    end
+    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
+end
 
 return function()
     for i, pin in pairs({ light.leds.b.pin, light.leds.c.pin, light.leds.h.pin }) do
