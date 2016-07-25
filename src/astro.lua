99e2c2c4e8f7a99b671ae78bc0ebcd46bb810c4d
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-25 16:09:20 +0300

    Plugin styled fix.

--- src/astro.lua
+++ src/astro.lua
@@ -8,37 +8,40 @@ local astro = {
         sunset = { hour = 20, minute = 0 },
         moonrise = { hour = 20, minute = 0 },
         moonset = { hour = 6, minute = 0 }
-    },
-    sync = function(callback)
-        http.get(astro.api.astronomy, nil, function(code, data)
-            if code == 200 then
-                local data_table = cjson.decode(data)
-
-                astro.data = {
-                    sunrise = {
-                        hour = tonumber(data_table.sun_phase.sunrise.hour),
-                        minute = tonumber(data_table.sun_phase.sunrise.minute)
-                    },
-                    sunset = {
-                        hour = tonumber(data_table.sun_phase.sunset.hour),
-                        minute = tonumber(data_table.sun_phase.sunset.minute)
-                    },
-                    moonrise = {
-                        hour = tonumber(data_table.moon_phase.moonrise.hour),
-                        minute = tonumber(data_table.moon_phase.moonrise.minute)
-                    },
-                    moonset = {
-                        hour = tonumber(data_table.moon_phase.moonset.hour),
-                        minute = tonumber(data_table.moon_phase.moonset.minute)
-                    }
-                }
-
-                if callback ~= nil then
-                    callback(astro)
-                end
-            end
-        end)
-    end
+    }
 }
 
-return astro
+function astro.sync(callback)
+    http.get(astro.api.astronomy, nil, function(code, data)
+        if code == 200 then
+            local data_table = cjson.decode(data)
+
+            astro.data = {
+                sunrise = {
+                    hour = tonumber(data_table.sun_phase.sunrise.hour),
+                    minute = tonumber(data_table.sun_phase.sunrise.minute)
+                },
+                sunset = {
+                    hour = tonumber(data_table.sun_phase.sunset.hour),
+                    minute = tonumber(data_table.sun_phase.sunset.minute)
+                },
+                moonrise = {
+                    hour = tonumber(data_table.moon_phase.moonrise.hour),
+                    minute = tonumber(data_table.moon_phase.moonrise.minute)
+                },
+                moonset = {
+                    hour = tonumber(data_table.moon_phase.moonset.hour),
+                    minute = tonumber(data_table.moon_phase.moonset.minute)
+                }
+            }
+
+            if callback ~= nil then
+                callback(astro)
+            end
+        end
+    end)
+end
+
+return function(app)
+    return astro
+end
