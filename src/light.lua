c16f7b2942ef5a4e2aa5217a3388efe96d841bda
Alex Melnyk <melnyk@codefire.com.ua>
2016-07-27 12:27:40 +0300

    Daylight algorithm added. Synchronization on modules loaded added.

--- src/light.lua
+++ src/light.lua
@@ -1,3 +1,4 @@
+local application
 local light = {
     const = { min = 0, max = 1023 },
     leds = {
@@ -14,54 +15,22 @@ local light = {
     },
     phase = { last = "", curr = "" }
 }
-light.transition = {}
 
-function light.transition.day()
-    print("DAY MODE")
-    for i = 0, light.const.max do
-        light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.const.min
-        light.leds.c.bright = light.leds.c.bright < light.bright.day and light.leds.c.bright + 1 or light.bright.day
-        light.leds.h.bright = light.leds.h.bright < light.bright.day and light.leds.h.bright + 1 or light.bright.day
+function light.sync()
+    local current, sunrise, sunset = application.modules.time.getTime().millis, application.modules.astro.data.sunrise.millis, application.modules.astro.data.sunset.millis
 
-        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
+    local daylight, absolute = sunset - sunrise, current - sunrise
+    local percent = absolute * 100. / daylight
+    -- 0 -> 50 -> 0
+    local lightvalue = 50 - math.abs(percent - 50)
 
-        tmr.delay(50)
+    if sunrise <= current and current <= sunset then
+        print(sunrise, current, sunset)
+        print(daylight, absolute, percent)
+        print(lightvalue)
+    else
+        print("MOON")
     end
-    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
-end
-
-function light.transition.moon()
-    print("MOON MODE")
-    for i = 0, light.const.max do
-        light.leds.b.bright = light.leds.b.bright < light.bright.moon.b and light.leds.b.bright + 1 or light.bright.moon.b
-        light.leds.c.bright = light.leds.c.bright > light.bright.moon.c and light.leds.c.bright - 1 or light.bright.moon.c
-        light.leds.h.bright = light.leds.h.bright > light.bright.moon.h and light.leds.h.bright - 1 or light.bright.moon.h
-
-        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
-
-        tmr.delay(50)
-    end
-    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
-end
-
-function light.transition.night()
-    print("NIGHT MODE")
-    for i = 0, light.const.max do
-        light.leds.b.bright = light.leds.b.bright > light.const.min and light.leds.b.bright - 1 or light.bright.night
-        light.leds.c.bright = light.leds.c.bright > light.const.min and light.leds.c.bright - 1 or light.bright.night
-        light.leds.h.bright = light.leds.h.bright > light.const.min and light.leds.h.bright - 1 or light.bright.night
-
-        pwm.setduty(light.leds.b.pin, light.leds.b.bright)
-        pwm.setduty(light.leds.c.pin, light.leds.c.bright)
-        pwm.setduty(light.leds.h.pin, light.leds.h.bright)
-
-        tmr.delay(50)
-    end
-    print(light.leds.b.bright, light.leds.c.bright, light.leds.h.bright)
 end
 
 function light.values()
@@ -77,11 +46,19 @@ function light.values()
     }
 end
 
-return function()
+return function(app)
+    application = app
+
     for i, pin in pairs({ light.leds.b.pin, light.leds.c.pin, light.leds.h.pin }) do
         pwm.setup(pin, 100, 0)
         pwm.start(pin)
     end
+
+    application.on("modulesloaded", function()
+        light.sync()
+
+        tmr.alarm(app.config.props.timers.light.id, app.config.props.timers.light.interval, tmr.ALARM_AUTO, light.sync)
+    end)
 
     return light
 end
